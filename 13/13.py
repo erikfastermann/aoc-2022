bf3e963c3c2354ff1b969ee9227808a853ffa359
Erik Fastermann <erik@fastermann.de>
2022-12-15 16:56:23 +0100

    day 13 cleanup

--- 13/13.py
+++ 13/13.py
@@ -1,32 +1,38 @@
 from pathlib import Path
-import re
-import functools
 import itertools
+import json
+import functools
 
-packets_raw = Path('input.txt').read_text().strip()
+packets_raw = Path('input.txt').read_text().splitlines()
+
+def separated_by_newline():
+    grouped = itertools.groupby(packets_raw, lambda x: x == '')
+    return (packets for k, packets in grouped if not k)
 
 def parse():
-    p = re.sub('^$', '],[', packets_raw, flags=re.MULTILINE)
-    p = re.sub('\]$', '],', p, flags=re.MULTILINE)
-    return eval(f'[[{p}]]') # unsafe for untrusted input
+    for pair in separated_by_newline():
+        yield [json.loads(packet) for packet in pair]
 
-packets = parse()
+packets = list(parse())
 
 def compare_int(x, y):
     return (x > y) - (x < y)
 
+def compare_list(x, y):
+    match (isinstance(x, list), isinstance(y, list)):
+        case (False, False):
+            return compare_int(x, y)
+        case (True, False):
+            return compare(x, [y])
+        case (False, True):
+            return compare([x], y)
+        case (True, True):
+            return compare(x, y)
+
 def compare(a, b):
     for x, y in zip(a, b):
-        match (isinstance(x, list), isinstance(y, list)):
-            case (False, False):
-                result = compare_int(x, y)
-            case (True, False):
-                result = compare(x, [y])
-            case (False, True):
-                result = compare([x], y)
-            case (True, True):
-                result = compare(x, y)
-        if result < 0 or result > 0:
+        result = compare_list(x, y)
+        if result != 0:
             return result
     return compare_int(len(a), len(b))
 
@@ -38,11 +44,27 @@ def flatten():
 
 DIVIDER_PACKETS = [[[2]], [[6]]]
 
+def binary_search(lst, v):
+    low = 0
+    high = len(lst) - 1
+    while low <= high:
+        mid = low + (high - low)//2
+        match compare(lst[mid], v):
+            case 0:
+                return mid
+            case -1:
+                low = mid + 1
+            case 1:
+                high = mid - 1
+            case _:
+                raise ValueError()
+    raise RuntimeError()
+
 def get_decoder_key():
     packets = list(itertools.chain(DIVIDER_PACKETS, flatten()))
     packets.sort(key=functools.cmp_to_key(compare))
-    i1 = packets.index(DIVIDER_PACKETS[0])
-    i2 = packets.index(DIVIDER_PACKETS[1])
+    i1 = binary_search(packets, DIVIDER_PACKETS[0])
+    i2 = binary_search(packets, DIVIDER_PACKETS[1])
     return (i1 + 1) * (i2 + 1)
 
 print(sum_sorted_index_pairs())
